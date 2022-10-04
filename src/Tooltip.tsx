60b1ab9b00b8a723a966772a2f37f9b40fedbcd2
Han Wang <hyw2@cornell.edu>
2022-10-04 01:11:12 -0700

    Update components

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -14,14 +14,9 @@ export function Tooltip({
   return (
     <span className="group z-10 inline relative">
       {underlineWhenTextOnly(children)}
-      <div className="hidden group-hover:flex max-w-full min-w-fit whitespace-nowrap absolute bottom-full left-1/2 mb-0.5 pb-1 -translate-x-1/2">
-        <div
-          className="relative bg-slate-800 text-center text-slate-50 text-xs px-1.5 py-1 rounded-lg border border-slate-50"
-          data-reach-alert="true"
-        >
-          {tip}
-        </div>
-      </div>
+      <span className="hidden group-hover:flex w-fit lg:whitespace-nowrap absolute bottom-full left-1/2 mb-0.5 pb-1 -translate-x-1/2 bg-slate-800 text-center text-slate-50 text-xs px-1.5 py-1 rounded-lg border border-slate-50">
+        {tip}
+      </span>
     </span>
   );
 }
