25699ef9c47bce3e08d085a6849a52634630ca78
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-10-20 18:50:23 -0400

    Show cursor pointer even if no colour is set

--- src/Card.tsx
+++ src/Card.tsx
@@ -49,7 +49,7 @@ export function Card({
     <Tag
       className={clsx(
         "block not-prose font-normal group relative my-2 ring-2 ring-transparent rounded-xl border border-slate-200 dark:border-slate-800 overflow-hidden px-6 py-5 w-full",
-        hoverHighlightColour && "cursor-pointer"
+        href || hoverHighlightColour ? "cursor-pointer" : undefined
       )}
       style={
         hoverHighlightColour && isHover
