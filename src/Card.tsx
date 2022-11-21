0e4d38c6ec7aa366fe0043010429215f7a5a7e03
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-11-21 10:26:54 -0500

    Keep original Card code

--- src/Card.tsx
+++ src/Card.tsx
@@ -27,51 +27,8 @@ export function Card({
 
   children?: ReactNode;
 }) {
-  return (
-    <div
-      className={clsx(
-        "block not-prose font-normal group relative my-2 ring-2 ring-transparent rounded-xl border border-slate-200 dark:border-slate-800 overflow-hidden w-full",
-        href && "cursor-pointer",
-        !href && "px-6 py-5", // Padding is set in OptionalLink when we have a link so the entire Card is clickable
-        className
-      )}
-      onClick={onClick}
-    >
-      <OptionalLink href={href}>
-        {icon ? <div className="h-6 w-6">{icon}</div> : null}
-        <h2
-          className={clsx(
-            "font-semibold text-base text-slate-800 dark:text-white",
-            icon !== null && icon !== undefined && "mt-4"
-          )}
-        >
-          {title}
-        </h2>
-        <span
-          className={clsx(
-            "mt-1 font-normal",
-            title
-              ? "text-slate-600 dark:text-slate-400"
-              : "text-slate-700 dark:text-slate-300"
-          )}
-        >
-          {children}
-        </span>
-      </OptionalLink>
-    </div>
-  );
-}
-
-function OptionalLink({
-  href,
-  children,
-}: {
-  href: string | undefined;
-  children: any;
-}) {
-  if (!href) {
-    return children;
-  }
+  // Use an <a> tag if we are linking to things
+  const Tag = href ? "a" : "div";
 
   const openLinksInNewTab = isAbsoluteUrl(href ?? "");
   const newTabProps = openLinksInNewTab
@@ -79,8 +36,35 @@ function OptionalLink({
     : {};
 
   return (
-    <a href={href} {...newTabProps} className="block px-6 py-5">
-      {children}
-    </a>
+    <Tag
+      className={clsx(
+        "block not-prose font-normal group relative my-2 ring-2 ring-transparent rounded-xl border border-slate-200 dark:border-slate-800 overflow-hidden px-6 py-5 w-full",
+        href && "cursor-pointer",
+        className
+      )}
+      href={href}
+      onClick={onClick}
+      {...newTabProps}
+    >
+      {icon ? <div className="h-6 w-6">{icon}</div> : null}
+      <h2
+        className={clsx(
+          "font-semibold text-base text-slate-800 dark:text-white",
+          icon !== null && icon !== undefined && "mt-4"
+        )}
+      >
+        {title}
+      </h2>
+      <span
+        className={clsx(
+          "mt-1 font-normal",
+          title
+            ? "text-slate-600 dark:text-slate-400"
+            : "text-slate-700 dark:text-slate-300"
+        )}
+      >
+        {children}
+      </span>
+    </Tag>
   );
 }
