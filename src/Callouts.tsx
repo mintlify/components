2e8c8e496f34c8d086d2efbb28f25cec2a841453
Han Wang <hyw2@cornell.edu>
2022-11-27 00:26:44 -0800

    Increase x padding

--- src/Callouts.tsx
+++ src/Callouts.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 
 type CalloutProps = {
@@ -8,7 +9,7 @@ type CalloutProps = {
 
 function CalloutTemplate({ children, icon, className }: CalloutProps) {
   return (
-    <div className={`px-4 py-4 mb-4 rounded-xl ${className}`}>
+    <div className={clsx("px-5 py-4 mb-4 rounded-xl", className)}>
       <div className="flex items-start space-x-3">
         <div className="mt-0.5 w-4">{icon}</div>
         <div className="flex-1 font-medium text-sm prose overflow-x-auto dark:text-slate-50">
