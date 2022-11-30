f8b9c0e88204e1553fbcde72214e9fc145855d49
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-11-30 12:48:14 -0500

    Delete commented out code and bump version

--- src/Api/ApiPlayground.tsx
+++ src/Api/ApiPlayground.tsx
@@ -139,12 +139,3 @@ export function ApiPlayground({
     </div>
   );
 }
-
-// <div className="py-3 px-3 max-h-60 whitespace-pre overflow-scroll border-t border-slate-200 dark:border-slate-700  dark:text-slate-300 font-mono text-xs leading-5">
-//           <span
-//             className="language-json max-h-72 overflow-scroll"
-//             dangerouslySetInnerHTML={{
-//               __html: apiResponse,
-//             }}
-//           />
-//         </div>
