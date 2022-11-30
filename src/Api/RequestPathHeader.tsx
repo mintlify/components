c8e7059d4f0f86edecb464e3f70eefbfff1b87eb
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-11-30 12:56:29 -0500

    Support undefined baseUrls array

--- src/Api/RequestPathHeader.tsx
+++ src/Api/RequestPathHeader.tsx
@@ -16,21 +16,23 @@ export const RequestPathHeader = ({
   path: string;
 
   /** Array of baseUrls to select from. Dropdown is hidden when there are zero or one options. */
-  baseUrls: string[];
+  baseUrls: string[] | undefined;
 
   /** What value of baseUrl the dropdown should show as selected before the user has changed the selection. */
-  defaultBaseUrl?: string;
+  defaultBaseUrl?: string | undefined;
 
   /** Callback when the user changes the baseUrl in the dropdown. */
   onBaseUrlChange: (baseUrl: string) => void;
 }) => (
   <div className="text-sm md:text-base flex items-center space-x-2 mb-2">
     <RequestMethodBubble method={method} />
-    <BaseUrlDropdown
-      baseUrls={baseUrls}
-      defaultValue={defaultBaseUrl}
-      onChange={onBaseUrlChange}
-    />
+    {baseUrls && (
+      <BaseUrlDropdown
+        baseUrls={baseUrls}
+        defaultValue={defaultBaseUrl}
+        onChange={onBaseUrlChange}
+      />
+    )}
     <div className="font-mono text-[0.95rem] overflow-auto">
       <p className="inline-block text-slate-700 dark:text-slate-100 font-semibold">
         {path}
