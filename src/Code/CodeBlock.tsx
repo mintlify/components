1ab984f53dccde5bf659aa7e55d867b1aae6efc8
Han Wang <hyw2@cornell.edu>
2022-12-07 11:49:32 -0800

    Revert "Update codeblock color"
    This reverts commit c5f4016e4b057e75c43bb600854a217aa8a14ca0.

--- src/Code/CodeBlock.tsx
+++ src/Code/CodeBlock.tsx
@@ -71,14 +71,14 @@ function CodeTabBar({
   children?: ReactElement;
 }) {
   return (
-    <div className="flex text-zinc-400 text-xs leading-6">
+    <div className="flex text-slate-400 text-xs leading-6">
       <div
         className="flex-none border-t border-b border-t-transparent px-4 py-1 flex items-center"
         style={{ color: filenameColor, borderBottomColor: filenameColor }}
       >
         {filename}
       </div>
-      <div className="flex-auto flex items-center bg-zinc-700/50 border border-zinc-500/30 rounded-t">
+      <div className="flex-auto flex items-center bg-slate-700/50 border border-slate-500/30 rounded-t">
         {children && (
           <div className="flex-auto flex items-center justify-end px-4 space-x-4">
             {children}
--- src/Code/CodeGroup.tsx
+++ src/Code/CodeGroup.tsx
@@ -74,7 +74,7 @@ export function CodeGroup({
         </Tab.List>
         <div
           className={clsx(
-            "flex-auto flex justify-end bg-zinc-700/50 border-y border-zinc-500/30 pr-4 rounded-tr",
+            "flex-auto flex justify-end bg-slate-700/50 border-y border-slate-500/30 pr-4 rounded-tr",
             selectedIndex === children.length - 1 ? "rounded-tl border-l" : ""
           )}
         >
@@ -131,7 +131,7 @@ function TabItem({
 
   return (
     <Tab
-      className="flex items-center relative overflow-hidden px-4 py-1 text-zinc-400 outline-none"
+      className="flex items-center relative overflow-hidden px-4 py-1 text-slate-400 outline-none"
       style={{ color: isSelected ? selectedColor : "" }}
     >
       <span className="z-10">{children}</span>
@@ -140,7 +140,7 @@ function TabItem({
       {!isSelected && (
         <TabAdornment
           className={clsx(
-            "bg-zinc-700/50 border-y border-slate-500/30",
+            "bg-slate-700/50 border-y border-slate-500/30",
             edges.leading === "capped" && "border-l rounded-tl",
             edges.trailing === "capped" && "border-r rounded-tr"
           )}
