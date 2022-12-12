f2320c4ecd5221568f0cd1ad648325d1806fee13
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-12-12 11:47:30 -0500

    Fix hydration error from null style value

--- src/Code/CodeGroup.tsx
+++ src/Code/CodeGroup.tsx
@@ -132,7 +132,7 @@ function TabItem({
   return (
     <Tab
       className="flex items-center relative overflow-hidden px-4 py-1 text-slate-400 outline-none"
-      style={{ color: isSelected ? selectedColor : "" }}
+      style={isSelected ? { color: selectedColor } : {}}
     >
       <span className="z-10">{children}</span>
 
