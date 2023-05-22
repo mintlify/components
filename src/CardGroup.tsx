37e23afa0d99542118cacd03ecc323c25bd5a56a
Nirmalya Ghosh <nirmalya.email@gmail.com>
2023-05-22 14:39:33 +0530

    Add className prop to CardGroup (#86)
    feat: adds className prop to CardGroup

--- src/CardGroup.tsx
+++ src/CardGroup.tsx
@@ -1,5 +1,16 @@
+import clsx from 'clsx';
 import React, { ReactNode } from 'react';
 
-export function CardGroup({ children, cols = 2 }: { children: ReactNode; cols?: 1 | 2 | 3 | 4 }) {
-  return <div className={`not-prose grid sm:grid-cols-${cols} gap-x-4`}>{children}</div>;
+export function CardGroup({
+  children,
+  cols = 2,
+  className,
+}: {
+  children: ReactNode;
+  cols?: 1 | 2 | 3 | 4;
+  className?: string;
+}) {
+  return (
+    <div className={clsx(`not-prose grid sm:grid-cols-${cols} gap-x-4`, className)}>{children}</div>
+  );
 }
