e41abbbb009c31c23a463a5e607e7098e15b1fcf
Kaishan (Sam) Ding <52273168+dks333@users.noreply.github.com>
2025-11-20 17:27:14 -0800

    add icons (#100)

--- packages/react/src/components/icon/index.ts
+++ packages/react/src/components/icon/index.ts
@@ -1,3 +1,5 @@
-export { Icon, getIconUrl, iconTypes, iconLibraries } from "./icon";
+export { Icon } from "./icon";
+export { BaseIcon, getIconUrl, iconTypes, iconLibraries } from "./base-icon";
 
 export type * from "./icon";
+export type * from "./base-icon";
