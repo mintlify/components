40e4bd993aac2f15d0a53b2b595d461fd04bd7af
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2026-01-14 15:55:50 -0800

    fix: return error for empty text in copyToClipboard

--- packages/components/src/utils/copyToClipboard.ts
+++ packages/components/src/utils/copyToClipboard.ts
@@ -3,6 +3,7 @@ export type CopyToClipboardResult = 'success' | 'error';
 export async function copyToClipboard(text: string): Promise<CopyToClipboardResult> {
   if (!text) {
     console.warn('Called copyToClipboard() with empty text');
+    return 'error';
   }
   try {
     await navigator.clipboard.writeText(text);
