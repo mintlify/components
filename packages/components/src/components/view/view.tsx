379ff0f15845244a58ed2e00133898fa561f1fba
dmytro <pqoqubbw@gmail.com>
2026-01-21 15:40:35 +0100

    feat: View component

--- packages/components/src/components/view/view.tsx
+++ packages/components/src/components/view/view.tsx
@@ -1,3 +1,35 @@
-export const View = () => {
-  return <div>View</div>;
+import { ComponentPropsWithoutRef, forwardRef, useMemo } from 'react';
+
+import { Classes } from '@/lib/local/selectors';
+import { MultiViewItemType } from '@/models';
+import { cn } from '@/utils/cn';
+
+type ViewPropsBase = {
+  title: string;
+  /** @internal Pass multiViewItems from useMDXContent() */
+  items: MultiViewItemType[];
 };
+
+export type ViewProps = ViewPropsBase & Omit<ComponentPropsWithoutRef<'div'>, keyof ViewPropsBase>;
+
+export const View = forwardRef<HTMLDivElement, ViewProps>(
+  ({ children, title, items, className, ...props }, ref) => {
+    const isVisible = useMemo(() => {
+      return items.find((item) => item.title === title)?.active;
+    }, [items, title]);
+
+    if (!isVisible) return null;
+
+    return (
+      <div
+        className={cn(Classes.MultiViewItem, 'prose prose-inverted', className)}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+View.displayName = 'View';
