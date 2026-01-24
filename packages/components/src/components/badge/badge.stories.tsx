0704f967855ebcc131c40b1565df883f34f32758
dmytro <pqoqubbw@gmail.com>
2026-01-24 17:03:15 +0100

    fix cursor comment

--- packages/components/src/components/badge/badge.stories.tsx
+++ packages/components/src/components/badge/badge.stories.tsx
@@ -13,7 +13,7 @@ const meta: Meta<typeof Badge> = {
     color: {
       control: "select",
       options: [
-        "stone",
+        "gray",
         "blue",
         "green",
         "orange",
