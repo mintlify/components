c4afc6936be63a32a4a0bb3584a1b316ce62a790
dmytro <pqoqubbw@gmail.com>
2026-01-30 23:39:50 +0100

    fix comment

--- packages/components/src/components/view/view.tsx
+++ packages/components/src/components/view/view.tsx
@@ -1,11 +1,7 @@
-import {
-  type ComponentPropsWithoutRef,
-  forwardRef,
-  useLayoutEffect,
-  useState,
-} from "react";
+import { type ComponentPropsWithoutRef, forwardRef, useState } from "react";
 
 import { Classes } from "@/constants/selectors";
+import { useIsomorphicLayoutEffect } from "@/hooks/use-isomorphic-layout-effect";
 import { cn } from "@/utils/cn";
 import type { MultiViewItemType } from "@/utils/icon-utils";
 
@@ -22,7 +18,7 @@ const View = forwardRef<HTMLDivElement, ViewProps>(
   ({ children, title, items, className, ...props }, ref) => {
     const [mounted, setMounted] = useState(false);
 
-    useLayoutEffect(() => {
+    useIsomorphicLayoutEffect(() => {
       setMounted(true);
     }, []);
 
