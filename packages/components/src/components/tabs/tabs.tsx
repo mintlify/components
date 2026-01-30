7a2546028f43a9585053b7cfcb137cb55c8303b5
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2026-01-30 14:51:32 -0800

    fmt

--- packages/components/src/components/tabs/tabs.tsx
+++ packages/components/src/components/tabs/tabs.tsx
@@ -8,9 +8,9 @@ import {
   useCallback,
   useEffect,
   useId,
+  useMemo,
   useRef,
   useState,
-  useMemo
 } from "react";
 import { Icon } from "@/components/icon";
 import {
@@ -70,18 +70,26 @@ const TabsRoot = ({
   const tabRefs = useRef<(HTMLLIElement | null)[]>([]);
   const uniqueId = useId();
 
-  const arrayChildren = useMemo(() => Children.toArray(children).filter(
-    (child): child is ReactElement<TabsItemProps> => isValidElement(child)
-  ), [children]);
+  const arrayChildren = useMemo(
+    () =>
+      Children.toArray(children).filter(
+        (child): child is ReactElement<TabsItemProps> => isValidElement(child)
+      ),
+    [children]
+  );
 
-  const tabIds = useMemo(() => arrayChildren.map((child, index) => {
-    if (child.props.id) {
-      return child.props.id;
-    }
+  const tabIds = useMemo(
+    () =>
+      arrayChildren.map((child, index) => {
+        if (child.props.id) {
+          return child.props.id;
+        }
 
-    const tabId = child.props.title ?? DEFAULT_TAB_TITLE;
-    return `${uniqueId}-${slugify(tabId)}-${index}`;
-  }), [arrayChildren, uniqueId]);
+        const tabId = child.props.title ?? DEFAULT_TAB_TITLE;
+        return `${uniqueId}-${slugify(tabId)}-${index}`;
+      }),
+    [arrayChildren, uniqueId]
+  );
 
   const validDefaultTabIndex =
     arrayChildren.length > 0
