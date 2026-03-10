f00d755eedf21d6f8f03f0f626799525d8c5ef82
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2026-03-10 13:27:29 -0700

    chore: export CodeSnippet component (#206)
    export codesnippet component

--- packages/components/src/components/code-group/index.ts
+++ packages/components/src/components/code-group/index.ts
@@ -1,2 +1,4 @@
 export type { CodeGroupProps } from "./code-group";
 export { CodeGroup } from "./code-group";
+export type { CodeSnippetProps } from "./code-snippet";
+export { CodeSnippet } from "./code-snippet";
